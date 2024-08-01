bc76ca981b55f7ae0df86cfbc8592ea183537e16
Ashish0634 <Hackerbaba.gov.in@gmail.com>
2024-08-01 11:42:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7402229500:AAEQbgarO2aPZvxTFK536JHacfRX-3FZE8I")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("up_65_ashish")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1040197289")
 
 
 
